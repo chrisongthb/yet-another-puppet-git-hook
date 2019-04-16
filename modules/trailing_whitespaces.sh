3bf1762f10e4aa3244f5e1912ab665ea7a57c70f
chrisongthb <6039434+chrisongthb@users.noreply.github.com>
2019-04-16 19:08:39 +0200

    Add new check (trailing whitespaces)

---
+++ modules/trailing_whitespaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+_puppet_git_hooks_check_trailing_whitespaces () {
+  local filteredfiles="$1"
+  local say_checkname='trailing whitespaces'
+
+  _puppet_git_hooks_say "checking" "$say_checkname"
+  if type grep > /dev/null 2>&1; then
+    if ! grep -qE ' $' ${filteredfiles}; then
+      _puppet_git_hooks_say "OK" "$say_checkname"
+    else
+      _puppet_git_hooks_say "nOK" "$say_checkname"
+      echo "Found trailing whitespaces in:"
+      grep -El ' $' ${filteredfiles}
+    fi
+  else
+    _puppet_git_hooks_say "FAILED" "$say_checkname"
+    echo "command not found: grep"
+  fi
+}
