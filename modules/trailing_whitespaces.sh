1eb4c72b1d48c0cacc6f4ba3e37077005d22a77d
chrisongthb <6039434+chrisongthb@users.noreply.github.com>
2019-05-09 15:51:31 +0200

    Exclude README.md from trailing whitespaces check

--- modules/trailing_whitespaces.sh
+++ modules/trailing_whitespaces.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 _puppet_git_hooks_check_trailing_whitespaces () {
-  local filteredfiles="$@"
+  local filteredfiles=
   local say_checkname='trailing whitespaces'
 
-  _puppet_git_hooks_say "checking" "$say_checkname"
-  if type grep > /dev/null 2>&1; then
-    if ! grep -qE ' $' ${filteredfiles}; then
-      _puppet_git_hooks_say "OK" "$say_checkname"
+  if filteredfiles=$(echo $@ | tr ' ' '\n' | grep -v "README.md"); then
+    _puppet_git_hooks_say "checking" "$say_checkname"
+    if type grep > /dev/null 2>&1; then
+      if ! grep -qE ' $' ${filteredfiles}; then
+        _puppet_git_hooks_say "OK" "$say_checkname"
+      else
+        _puppet_git_hooks_say "nOK" "$say_checkname"
+        echo "Found trailing whitespaces in:"
+        grep --color=auto -HnoE ' $' ${filteredfiles}
+      fi
     else
-      _puppet_git_hooks_say "nOK" "$say_checkname"
-      echo "Found trailing whitespaces in:"
-      grep --color=auto -HnoE ' $' ${filteredfiles}
+      _puppet_git_hooks_say "FAILED" "$say_checkname"
+      echo "command not found: grep"
     fi
-  else
-    _puppet_git_hooks_say "FAILED" "$say_checkname"
-    echo "command not found: grep"
   fi
 }
